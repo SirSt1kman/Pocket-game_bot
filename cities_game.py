b3e4ff7e33aef85def95476cfa0037d012df9e36
Mark Naumets <120276397+SirSt1kman@users.noreply.github.com>
2025-05-13 15:31:42 +0300

    fixed bugs

--- cities_game.py
+++ cities_game.py
@@ -4,10 +4,6 @@ from cities_data import cities_list
 def cities_loop(city: str, last_char: str, f: bool) -> str:
     """Функция, которая проверяет введённое слово на корректность и выходит из игры по её окончании"""
 
-    # Проверка на то, закончилась ли игра
-    if not f:
-        return 'game over'
-
     # сыграть ещё раз
     if city == 'play':
         return 'play'
@@ -16,6 +12,10 @@ def cities_loop(city: str, last_char: str, f: bool) -> str:
     if city == 'выход':
         return 'exit'
 
+    # Проверка на то, закончилась ли игра
+    if not f:
+        return 'game over'
+        
     # Предупреждение о том, что введен не город, а другое слово
     if city not in cities_list:
         return 'not city'
@@ -26,6 +26,7 @@ def cities_loop(city: str, last_char: str, f: bool) -> str:
 
     # Ветка для начала игры, когда последней буквы ещё нет
     if last_char == '1':
+        unnamed_cities.remove(city)
         return 'ok'
 
     # Предупреждение о том, что названный город начинается не с той буквы
@@ -65,7 +66,7 @@ def choose_random_city(last_char: str) -> str:
 def new_game() -> None:
     """Начало новой игры"""
     global unnamed_cities
-    unnamed_cities = cities_list
+    unnamed_cities = cities_list[:]
 
 
-unnamed_cities = cities_list
+unnamed_cities = cities_list[:]
