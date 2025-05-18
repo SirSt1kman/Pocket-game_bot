0a32e50f6ca4c0c5064adfef5808f6e8faed7fb6
Mark Naumets <120276397+SirSt1kman@users.noreply.github.com>
2025-05-18 12:42:42 +0300

    Update test_data.py

--- test_data.py
+++ test_data.py
@@ -1,1 +1,7 @@
-cities_list = ['аб', 'ап', 'бв', 'вп', 'де', 'пк', 'ка', 'кв', 'вд', 'ед', 'еа', 'пп', 'кбы', 'абёь']
+cities_lis = {'1': [1, 2], '2': [3, 4], '3': [5, 6]}
+cop = {}
+for key in cities_lis:
+    cop[key] = cities_lis[key][:]
+cop['1'].remove(1)
+print(cop)
+print(cities_lis)
